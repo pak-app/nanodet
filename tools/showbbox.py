3528518ff48c8ab9afaa9535395b5c29eef2c977
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2024-08-01 23:43:58 +0330

    edit show bounding box

--- tools/showbbox.py
+++ tools/showbbox.py
@@ -14,34 +14,48 @@ def create_boxes(img_size, norm_ann):
 
     return (x, y), (x + w, y + h)
 
+img_addr = r"D:\Workspace\Work\Zhivan\models\nanodet\datasets\data\RaccoonYD-38\test\raccoon-6_jpg.rf.e1ef482779f9ef651ec62ed3a9c1e2d7.jpg"
+real_ann_addr = r"D:\Workspace\Work\Zhivan\models\nanodet\datasets\data\RaccoonYD-38\test\raccoon-6_jpg.rf.e1ef482779f9ef651ec62ed3a9c1e2d7.txt"
+pred_ann_addr = r"D:\Workspace\Work\Zhivan\models\nanodet\workspace\nanodet-plus-m_416_Raccoon\20240801091605\results.json"
 
 if __name__ == "__main__":
 
-    img = cv2.imread(r'C:\Users\p.alishah\Desktop\Projects\python\models\nanodet\datasets\data\RaccoonYD-38\test\raccoon-6_jpg.rf.e1ef482779f9ef651ec62ed3a9c1e2d7.jpg')
+    img = cv2.imread(img_addr)
     
     # Load data from a local JSON file
-    with open(r'C:\Users\p.alishah\Desktop\Projects\python\models\nanodet\workspace\nanodet-plus-m_416_Raccoon\20240801091605\results.json', 'r') as f:
+    with open(pred_ann_addr, 'r') as f:
         ann_pred = json.load(f)
     
-    with open(r"C:\Users\p.alishah\Desktop\Projects\python\models\nanodet\datasets\data\RaccoonYD-38\test\raccoon-6_jpg.rf.e1ef482779f9ef651ec62ed3a9c1e2d7.txt", 'r') as file:
+    with open(real_ann_addr, 'r') as file:
         ann_real = file.read().split()
     
     
     ann_real = list(map(float, ann_real[1:]))
-    
+    cv2.imshow("imgshow", img)
     # Scale annotations
     p1, p2 = create_boxes(img.shape, ann_real)
 
-    # Load the image
-    print("Predicted annotatioins:", ann_pred[0]['bbox'])
+    
     # Define bounding box coordinates
     x, y, w, h = ann_pred[0]['bbox']
     
+    print("cordinate of real bounding box:")
+    print("P1:", p1)
+    print("P2:", p2)
+
+    print("cordiante of bounding box:")
+    print("P1:", int(x), int(y))
+    print("P2:", int(w), int(h))
+
     # Draw the bounding box
     cv2.rectangle(img, (int(x), int(y)), (int(w), int(h)), (0, 255, 0), 2)
+    cv2.rectangle(img, (int(y), int(x)), (int(w), int(h)), (0, 0, 255), 2)
     cv2.rectangle(img, p1, p2, (255, 0, 0), 2)
 
     # Display the image
     cv2.imshow('Image with Bounding Box', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.imshow("Image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
