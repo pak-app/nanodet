2fb57588740b5c6a086c2c42e91da2c4b624dcbb
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2024-08-01 09:19:41 +0000

    add test config file and test images

---
+++ test.zsh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+python tools/test.py --task test --model /teamspace/studios/this_studio/nanodet/workspace/nanodet-plus-m_416_Raccoon/model_best/model_best.ckpt --config /teamspace/studios/this_studio/nanodet/datasets/configs/RaccoonYD-38/config_test.yml
